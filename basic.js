833f5383da6d6f4aed24d84ddc93081f7411a9a7
propatzer <114686326+propatzer@users.noreply.github.com>
2023-01-24 22:50:12 -0500

    updated button links

--- basic.js
+++ basic.js
@@ -35,33 +35,8 @@ const doArt = () => {
 
 doArt();
 
-var slidePosition = 1;
-SlideShow(slidePosition);
+//create slideshow
 
-// forward/Back controls
-function plusSlides(n) {
-  SlideShow(slidePosition += n);
-}
 
-//  images controls
-function currentSlide(n) {
-  SlideShow(slidePosition = n);
-}
-
-function SlideShow(n) {
-  var i;
-  var slides = document.getElementsByClassName("Containers");
-  var circles = document.getElementsByClassName("dots");
-  if (n > slides.length) {slidePosition = 1}
-  if (n < 1) {slidePosition = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-  }
-  for (i = 0; i < circles.length; i++) {
-      circles[i].className = circles[i].className.replace(" enable", "");
-  }
-  slides[slidePosition-1].style.display = "block";
-  circles[slidePosition-1].className += " enable";
-} 
 
 // Path: index.html
